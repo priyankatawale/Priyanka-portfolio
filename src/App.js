561edb5e0863e04ebe1ba2356f6115645ee8492b
priyanka-zotalabs <65708273+priyanka-zotalabs@users.noreply.github.com>
2021-07-11 08:22:43 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import './App.css';
 import img from './image/test1.png'
 
 function App() {
+
+
+  var name= "Priyanka"; 
+  var a,b,sum;
+  a=10;
+  b=20;
+  sum=a+b;
+
+  const openSum=()=>{
+
+    console.log("Hello Vaibhav",name,sum)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={img} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to this page.
         </p>
-        <p>Vaibhav</p>
+        <button onClick={openSum} >Click here</button>
+        <p>{name}</p>
+        <h1>{a}+{b}={sum}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
